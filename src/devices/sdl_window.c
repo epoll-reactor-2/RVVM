f36ad644a843b16541dfa9d30ea3ac80a4af5d4e
LekKit <50500857+LekKit@users.noreply.github.com>
2024-11-05 19:59:16 +0200

    sdl_window: Use MPL 2.0 license header

--- src/devices/sdl_window.c
+++ src/devices/sdl_window.c
@@ -5,13 +5,6 @@ Copyright (C) 2022  LekKit <github.com/LekKit>
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-Alternatively, the contents of this file may be used under the terms
-of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or any later version.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "gui_window.h"
