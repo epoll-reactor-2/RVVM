e47efb9dfab8d21f7c6e2e8e0401678c75b098af
LekKit <50500857+LekKit@users.noreply.github.com>
2024-11-05 19:56:11 +0200

    i2c-oc: Use MPL 2.0 license header

--- src/devices/i2c-oc.h
+++ src/devices/i2c-oc.h
@@ -2,18 +2,9 @@
 i2c-oc.h - OpenCores I2C Controller
 Copyright (C) 2022  LekKit <github.com/LekKit>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
 #ifndef RVVM_I2C_OC_H
@@ -30,7 +21,7 @@ typedef struct {
     uint16_t addr;
     // Device-specific data
     void*    data;
-    
+
     // Start transaction, return device availability
     bool (*start)(void* dev, bool is_write);
     // Return false on NACK or no data to read
