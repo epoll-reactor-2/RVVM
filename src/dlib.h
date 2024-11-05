2102c63b28d2a5405460c16cc70ca29ac445edfd
0xCatPKG <0xCatPKG@rvvm.dev>
2024-11-05 14:55:41 -0300

    dlib: Use MPL 2.0 license header

--- src/dlib.h
+++ src/dlib.h
@@ -1,19 +1,11 @@
 /*
 dlib.h - Dynamic library loader
-Copyright (C) 2023 0xCatPKG <github.com/0xCatPKG>
+Copyright (C) 2023  0xCatPKG <0xCatPKG@rvvm.dev>
+                    0xCatPKG <github.com/0xCatPKG>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
 #ifndef RVVM_DLIB_H
