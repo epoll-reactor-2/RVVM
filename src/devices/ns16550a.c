a584992c0dc61c792b40c1b383fde680bac47e92
LekKit <50500857+LekKit@users.noreply.github.com>
2024-11-08 19:59:49 +0200

    ns16550a: Use MPL 2.0 license header

--- src/devices/ns16550a.c
+++ src/devices/ns16550a.c
@@ -1,20 +1,10 @@
 /*
 ns16550a.c - NS16550A UART
 Copyright (C) 2021  LekKit <github.com/LekKit>
-                    Mr0maks <mr.maks0443@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
 #include "ns16550a.h"
