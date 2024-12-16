9c03a2224e322ef97511dc76b38cbfb03e805076
LekKit <50500857+LekKit@users.noreply.github.com>
2024-12-16 23:54:26 +0200

    rvvm_types: Remove obscure types
    - Remove virt_addr_t and phys_addr_t, those typenames collide with Haiku headers and in general rvvm_addr_t should be used instead
    - Remove maxlen_t and other similar types, this should be defined in rvvm.h
    - Remove vmptr_t as it has seen little use

--- src/rvvm_types.h
+++ src/rvvm_types.h
@@ -26,39 +26,13 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #define PRIx64 "llx"
 #endif
 
-#ifndef PRIx32
-#define PRIx32 "x"
-#endif
-
 #ifdef __SIZEOF_INT128__
 #define INT128_SUPPORT 1
 typedef unsigned __int128 uint128_t;
 typedef __int128 int128_t;
 #endif
 
-// Max XLEN/SXLEN values
-#ifdef USE_RV64
-typedef uint64_t maxlen_t;
-typedef int64_t smaxlen_t;
-#define MAX_XLEN 64
-#define MAX_SHAMT_BITS 6
-#define PRIxXLEN PRIx64
-#else
-typedef uint32_t maxlen_t;
-typedef int32_t smaxlen_t;
-#define MAX_XLEN 32
-#define MAX_SHAMT_BITS 5
-#define PRIxXLEN PRIx32
-#endif
-
-typedef double fmaxlen_t;
-
-// Distinguish between virtual and physical addresses
-typedef maxlen_t virt_addr_t;
-typedef maxlen_t phys_addr_t;
-
 typedef uint8_t regid_t;  // Register index
 typedef uint8_t bitcnt_t; // Bits count
-typedef uint8_t* vmptr_t; // Pointer to VM memory
 
 #endif
