194b6ab9394364e1a5db4f9a8ecd36185bf1e8a4
LekKit <50500857+LekKit@users.noreply.github.com>
2024-12-17 00:21:35 +0200

    mem_ops: Refactoring

--- src/mem_ops.h
+++ src/mem_ops.h
@@ -1,5 +1,5 @@
 /*
-mem_ops.h - memory operations functions
+mem_ops.h - Explicit memory intrinsics
 Copyright (C) 2021  LekKit <github.com/LekKit>
 
 This Source Code Form is subject to the terms of the Mozilla Public
@@ -7,12 +7,13 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-#ifndef RISCV_MEM_OPS_H
-#define RISCV_MEM_OPS_H
+#ifndef LEKKIT_MEM_OPS_H
+#define LEKKIT_MEM_OPS_H
 
-#include "compiler.h"
 #include <stdint.h>
 #include <memory.h>
+
+#include "compiler.h"
 
 /*
  * Simple memory operations (write, read integers) for internal usage,
@@ -24,18 +25,22 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
  * crashes on old ARM CPUs, etc
  */
 
-static inline uint64_t read_uint64_le_m(const void* addr) {
+static forceinline uint64_t read_uint64_le_m(const void* addr) {
     const uint8_t* arr = (const uint8_t*)addr;
-    return (uint64_t)arr[0] | ((uint64_t)arr[1] << 8)
-    | ((uint64_t)arr[2] << 16) | ((uint64_t)arr[3] << 24)
-    | ((uint64_t)arr[4] << 32) | ((uint64_t)arr[5] << 40)
-    | ((uint64_t)arr[6] << 48) | ((uint64_t)arr[7] << 56);
+    return ((uint64_t)arr[0])
+        | (((uint64_t)arr[1]) << 8)
+        | (((uint64_t)arr[2]) << 16)
+        | (((uint64_t)arr[3]) << 24)
+        | (((uint64_t)arr[4]) << 32)
+        | (((uint64_t)arr[5]) << 40)
+        | (((uint64_t)arr[6]) << 48)
+        | (((uint64_t)arr[7]) << 56);
 }
 
-static inline void write_uint64_le_m(void* addr, uint64_t val) {
+static forceinline void write_uint64_le_m(void* addr, uint64_t val) {
     uint8_t* arr = (uint8_t*)addr;
     arr[0] = val & 0xFF;
-    arr[1] = (val >> 8) & 0xFF;
+    arr[1] = (val >> 8)  & 0xFF;
     arr[2] = (val >> 16) & 0xFF;
     arr[3] = (val >> 24) & 0xFF;
     arr[4] = (val >> 32) & 0xFF;
@@ -44,26 +49,28 @@ static inline void write_uint64_le_m(void* addr, uint64_t val) {
     arr[7] = (val >> 56) & 0xFF;
 }
 
-static inline uint32_t read_uint32_le_m(const void* addr) {
+static forceinline uint32_t read_uint32_le_m(const void* addr) {
     const uint8_t* arr = (const uint8_t*)addr;
-    return (uint32_t)arr[0] | ((uint32_t)arr[1] << 8)
-    | ((uint32_t)arr[2] << 16) | ((uint32_t)arr[3] << 24);
+    return ((uint32_t)arr[0])
+        | (((uint32_t)arr[1]) << 8)
+        | (((uint32_t)arr[2]) << 16)
+        | (((uint32_t)arr[3]) << 24);
 }
 
-static inline void write_uint32_le_m(void* addr, uint32_t val) {
+static forceinline void write_uint32_le_m(void* addr, uint32_t val) {
     uint8_t* arr = (uint8_t*)addr;
     arr[0] = val & 0xFF;
-    arr[1] = (val >> 8) & 0xFF;
+    arr[1] = (val >> 8)  & 0xFF;
     arr[2] = (val >> 16) & 0xFF;
     arr[3] = (val >> 24) & 0xFF;
 }
 
-static inline uint16_t read_uint16_le_m(const void* addr) {
+static forceinline uint16_t read_uint16_le_m(const void* addr) {
     const uint8_t* arr = (const uint8_t*)addr;
-    return (uint16_t)arr[0] | ((uint16_t)arr[1] << 8);
+    return ((uint16_t)arr[0]) | (((uint16_t)arr[1]) << 8);
 }
 
-static inline void write_uint16_le_m(void* addr, uint16_t val) {
+static forceinline void write_uint16_le_m(void* addr, uint16_t val) {
     uint8_t* arr = (uint8_t*)addr;
     arr[0] = val & 0xFF;
     arr[1] = (val >> 8) & 0xFF;
@@ -73,18 +80,22 @@ static inline void write_uint16_le_m(void* addr, uint16_t val) {
  * Big-endian operations
  */
 
-static inline uint64_t read_uint64_be_m(const void* addr) {
+static forceinline uint64_t read_uint64_be_m(const void* addr) {
     const uint8_t* arr = (const uint8_t*)addr;
-    return (uint64_t)arr[7] | ((uint64_t)arr[6] << 8)
-    | ((uint64_t)arr[5] << 16) | ((uint64_t)arr[4] << 24)
-    | ((uint64_t)arr[3] << 32) | ((uint64_t)arr[2] << 40)
-    | ((uint64_t)arr[1] << 48) | ((uint64_t)arr[0] << 56);
+    return ((uint64_t)arr[7])
+        | (((uint64_t)arr[6]) << 8)
+        | (((uint64_t)arr[5]) << 16)
+        | (((uint64_t)arr[4]) << 24)
+        | (((uint64_t)arr[3]) << 32)
+        | (((uint64_t)arr[2]) << 40)
+        | (((uint64_t)arr[1]) << 48)
+        | (((uint64_t)arr[0]) << 56);
 }
 
-static inline void write_uint64_be_m(void* addr, uint64_t val) {
+static forceinline void write_uint64_be_m(void* addr, uint64_t val) {
     uint8_t* arr = (uint8_t*)addr;
     arr[7] = val & 0xFF;
-    arr[6] = (val >> 8) & 0xFF;
+    arr[6] = (val >> 8)  & 0xFF;
     arr[5] = (val >> 16) & 0xFF;
     arr[4] = (val >> 24) & 0xFF;
     arr[3] = (val >> 32) & 0xFF;
@@ -93,26 +104,28 @@ static inline void write_uint64_be_m(void* addr, uint64_t val) {
     arr[0] = (val >> 56) & 0xFF;
 }
 
-static inline uint32_t read_uint32_be_m(const void* addr) {
+static forceinline uint32_t read_uint32_be_m(const void* addr) {
     const uint8_t* arr = (const uint8_t*)addr;
-    return (uint32_t)arr[3] | ((uint32_t)arr[2] << 8)
-    | ((uint32_t)arr[1] << 16) | ((uint32_t)arr[0] << 24);
+    return ((uint32_t)arr[3])
+        | (((uint32_t)arr[2]) << 8)
+        | (((uint32_t)arr[1]) << 16)
+        | (((uint32_t)arr[0]) << 24);
 }
 
-static inline void write_uint32_be_m(void* addr, uint32_t val) {
+static forceinline void write_uint32_be_m(void* addr, uint32_t val) {
     uint8_t* arr = (uint8_t*)addr;
     arr[3] = val & 0xFF;
-    arr[2] = (val >> 8) & 0xFF;
+    arr[2] = (val >> 8)  & 0xFF;
     arr[1] = (val >> 16) & 0xFF;
     arr[0] = (val >> 24) & 0xFF;
 }
 
-static inline uint16_t read_uint16_be_m(const void* addr) {
+static forceinline uint16_t read_uint16_be_m(const void* addr) {
     const uint8_t* arr = (const uint8_t*)addr;
-    return (uint16_t)arr[1] | ((uint16_t)arr[0] << 8);
+    return ((uint16_t)arr[1]) | (((uint16_t)arr[0]) << 8);
 }
 
-static inline void write_uint16_be_m(void* addr, uint16_t val) {
+static forceinline void write_uint16_be_m(void* addr, uint16_t val) {
     uint8_t* arr = (uint8_t*)addr;
     arr[1] = val & 0xFF;
     arr[0] = (val >> 8) & 0xFF;
@@ -183,17 +196,23 @@ TSAN_SUPPRESS static forceinline void write_uint8(void* addr, uint8_t val) {
  * Floating-point memory operations (misaligned)
  */
 
-static inline float read_float_le_m(const void *addr) {
-    uint32_t i_v = read_uint32_le_m(addr);
-    float ret;
-    memcpy(&ret, &i_v, sizeof(float));
+static inline double read_double_le_m(const void* addr) {
+    double ret;
+    uint64_t i_v = read_uint64_le_m(addr);
+    memcpy(&ret, &i_v, sizeof(double));
     return ret;
 }
 
-static inline double read_double_le_m(const void *addr) {
-    uint64_t i_v = read_uint64_le_m(addr);
-    double ret;
-    memcpy(&ret, &i_v, sizeof(double));
+static inline void write_double_le_m(void* addr, double val) {
+    uint64_t i_v;
+    memcpy(&i_v, &val, sizeof(i_v));
+    write_uint64_le_m(addr, i_v);
+}
+
+static inline float read_float_le_m(const void* addr) {
+    float ret;
+    uint32_t i_v = read_uint32_le_m(addr);
+    memcpy(&ret, &i_v, sizeof(float));
     return ret;
 }
 
@@ -203,17 +222,11 @@ static inline void write_float_le_m(void* addr, float val) {
     write_uint32_le_m(addr, i_v);
 }
 
-static inline void write_double_le_m(void* addr, double val) {
-    uint64_t i_v;
-    memcpy(&i_v, &val, sizeof(i_v));
-    write_uint64_le_m(addr, i_v);
-}
-
 /*
  * Floating-point memory operations (aligned)
  */
 
-TSAN_SUPPRESS static forceinline float read_float_le(const void *addr) {
+TSAN_SUPPRESS static forceinline float read_float_le(const void* addr) {
 #ifdef HOST_LITTLE_ENDIAN
     return *(const float*)addr;
 #else
@@ -221,7 +234,7 @@ TSAN_SUPPRESS static forceinline float read_float_le(const void *addr) {
 #endif
 }
 
-TSAN_SUPPRESS static forceinline double read_double_le(const void *addr) {
+TSAN_SUPPRESS static forceinline double read_double_le(const void* addr) {
 #ifdef HOST_LITTLE_ENDIAN
     return *(const double*)addr;
 #else
@@ -229,7 +242,7 @@ TSAN_SUPPRESS static forceinline double read_double_le(const void *addr) {
 #endif
 }
 
-TSAN_SUPPRESS static forceinline void write_float_le(void *addr, float val) {
+TSAN_SUPPRESS static forceinline void write_float_le(void* addr, float val) {
 #ifdef HOST_LITTLE_ENDIAN
     *(float*)addr = val;
 #else
@@ -237,7 +250,7 @@ TSAN_SUPPRESS static forceinline void write_float_le(void *addr, float val) {
 #endif
 }
 
-TSAN_SUPPRESS static forceinline void write_double_le(void *addr, double val) {
+TSAN_SUPPRESS static forceinline void write_double_le(void* addr, double val) {
 #ifdef HOST_LITTLE_ENDIAN
     *(double*)addr = val;
 #else
