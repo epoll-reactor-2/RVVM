66a1110a8b3817092ce9d073787494d1b45e2ef5
LekKit <50500857+LekKit@users.noreply.github.com>
2024-12-17 00:14:29 +0200

    riscv_atomics: Internal renaming, use simpler LR/SC handling
    - Previous "reservation tracking" is broken anyways on platforms without native LR/SC, but it was also slower

--- src/cpu/riscv_atomics.h
+++ src/cpu/riscv_atomics.h
@@ -22,54 +22,41 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #define RISCV_AMO_MINU 0x18
 #define RISCV_AMO_MAXU 0x1C
 
-static void riscv_invalidate_lrsc(rvvm_hart_t *vm)
-{
-    vector_foreach(vm->machine->harts, i) {
-        rvvm_hart_t* hart = vector_at(vm->machine->harts, i);
-        if (hart != vm) {
-            atomic_store_uint32(&hart->lrsc, false);
-        }
-    }
-}
-
-static forceinline void riscv_emulate_atomic_w(rvvm_hart_t *vm, const uint32_t insn)
+static forceinline void riscv_emulate_atomic_w(rvvm_hart_t* vm, const uint32_t insn)
 {
     const uint32_t op = insn >> 27;
     const regid_t rds = bit_cut(insn, 7, 5);
     const regid_t rs1 = bit_cut(insn, 15, 5);
     const regid_t rs2 = bit_cut(insn, 20, 5);
-    const xaddr_t addr = riscv_read_reg(vm, rs1);
+    const xaddr_t vaddr = riscv_read_reg(vm, rs1);
     const uint32_t val = riscv_read_reg(vm, rs2);
-    uint8_t buff[4]; // MMIO atomics bounce buffer
+    // MMIO atomics bounce buffer
+    uint32_t bounce;
 
-    if (unlikely(addr & 3)) {
-        riscv_trap(vm, TRAP_STORE_MISALIGN, 0);
+    if (unlikely(vaddr & 3)) {
+        riscv_trap(vm, RISCV_TRAP_STORE_MISALIGN, vaddr);
         return;
     }
 
-    void* ptr = riscv_vma_translate_w(vm, addr, buff, sizeof(buff));
+    void* ptr = riscv_rmw_translate(vm, vaddr, &bounce, sizeof(bounce));
     if (unlikely(ptr == NULL)) return;
 
     switch (op) {
         case RISCV_AMO_LR:
             // Mark our reservation
-            atomic_store_uint32(&vm->lrsc, true);
+            vm->lrsc = true;
+            vm->lrsc_addr = vaddr;
             vm->lrsc_cas = atomic_load_uint32_le(ptr);
             riscv_write_reg(vm, rds, (int32_t)vm->lrsc_cas);
             break;
         case RISCV_AMO_SC:
-            // Invalidate all other reservations
-            riscv_invalidate_lrsc(vm);
-
             // If our reservation is valid, perform a CAS
-            if (atomic_load_uint32(&vm->lrsc) && atomic_cas_uint32_le(ptr, vm->lrsc_cas, val)) {
+            if (vm->lrsc && vm->lrsc_addr == vaddr && atomic_cas_uint32_le(ptr, vm->lrsc_cas, val)) {
                 riscv_write_reg(vm, rds, 0);
             } else {
                 riscv_write_reg(vm, rds, 1);
             }
-
-            // Invalidate this hart reservation
-            atomic_store_uint32(&vm->lrsc, false);
+            vm->lrsc = false;
             break;
         case RISCV_AMO_SWAP:
             riscv_write_reg(vm, rds, (int32_t)atomic_swap_uint32_le(ptr, val));
@@ -103,86 +90,83 @@ static forceinline void riscv_emulate_atomic_w(rvvm_hart_t *vm, const uint32_t i
             break;
     }
 
-    if (unlikely(ptr == buff)) {
-        riscv_mmu_vma_mmio_write(vm, addr, buff, sizeof(buff));
+    if (unlikely(ptr == &bounce)) {
+        riscv_rmw_mmio_write(vm, vaddr, &bounce, sizeof(bounce));
     }
 }
 
 #ifdef RV64
 
-static forceinline void riscv_emulate_atomic_d(rvvm_hart_t *vm, const uint32_t insn)
+static forceinline void riscv_emulate_atomic_d(rvvm_hart_t* vm, const uint32_t insn)
 {
     const uint32_t op = insn >> 27;
     const regid_t rds = bit_cut(insn, 7, 5);
     const regid_t rs1 = bit_cut(insn, 15, 5);
     const regid_t rs2 = bit_cut(insn, 20, 5);
-    const xaddr_t addr = riscv_read_reg(vm, rs1);
+    const xaddr_t vaddr = riscv_read_reg(vm, rs1);
     const uint64_t val = riscv_read_reg(vm, rs2);
-    uint8_t buff[8]; // MMIO atomics bounce buffer
+    // MMIO atomics bounce buffer
+    uint64_t bounce;
 
-    if (unlikely(addr & 7)) {
-        riscv_trap(vm, TRAP_STORE_MISALIGN, 0);
+    if (unlikely(vaddr & 7)) {
+        riscv_trap(vm, RISCV_TRAP_STORE_MISALIGN, vaddr);
         return;
     }
 
-    void* ptr = riscv_vma_translate_w(vm, addr, buff, sizeof(buff));
+    void* ptr = riscv_rmw_translate(vm, vaddr, &bounce, sizeof(bounce));
     if (unlikely(ptr == NULL)) return;
 
     switch (op) {
         case RISCV_AMO_LR:
             // Mark our reservation
-            atomic_store_uint32(&vm->lrsc, true);
+            vm->lrsc = true;
+            vm->lrsc_addr = vaddr;
             vm->lrsc_cas = atomic_load_uint64_le(ptr);
-            vm->registers[rds] = vm->lrsc_cas;
+            riscv_write_reg(vm, rds, vm->lrsc_cas);
             break;
         case RISCV_AMO_SC:
-            // Invalidate all other reservations
-            riscv_invalidate_lrsc(vm);
-
             // If our reservation is valid, perform a CAS
-            if (atomic_load_uint32(&vm->lrsc) && atomic_cas_uint64_le(ptr, vm->lrsc_cas, val)) {
+            if (vm->lrsc && vm->lrsc_addr == vaddr && atomic_cas_uint64_le(ptr, vm->lrsc_cas, val)) {
                 riscv_write_reg(vm, rds, 0);
             } else {
                 riscv_write_reg(vm, rds, 1);
             }
-
-            // Invalidate this hart reservation
-            atomic_store_uint32(&vm->lrsc, false);
+            vm->lrsc = false;
             break;
         case RISCV_AMO_SWAP:
-            vm->registers[rds] = atomic_swap_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_swap_uint64_le(ptr, val));
             break;
         case RISCV_AMO_ADD:
-            vm->registers[rds] = atomic_add_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_add_uint64_le(ptr, val));
             break;
         case RISCV_AMO_XOR:
-            vm->registers[rds] = atomic_xor_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_xor_uint64_le(ptr, val));
             break;
         case RISCV_AMO_AND:
-            vm->registers[rds] = atomic_and_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_and_uint64_le(ptr, val));
             break;
         case RISCV_AMO_OR:
-            vm->registers[rds] = atomic_or_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_or_uint64_le(ptr, val));
             break;
         case RISCV_AMO_MIN:
-            vm->registers[rds] = atomic_min_int64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_min_int64_le(ptr, val));
             break;
         case RISCV_AMO_MAX:
-            vm->registers[rds] = atomic_max_int64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_max_int64_le(ptr, val));
             break;
         case RISCV_AMO_MINU:
-            vm->registers[rds] = atomic_minu_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_minu_uint64_le(ptr, val));
             break;
         case RISCV_AMO_MAXU:
-            vm->registers[rds] = atomic_maxu_uint64_le(ptr, val);
+            riscv_write_reg(vm, rds, atomic_maxu_uint64_le(ptr, val));
             break;
         default:
             riscv_illegal_insn(vm, insn);
             break;
     }
 
-    if (unlikely(ptr == buff)) {
-        riscv_mmu_vma_mmio_write(vm, addr, buff, sizeof(buff));
+    if (unlikely(ptr == &bounce)) {
+        riscv_rmw_mmio_write(vm, vaddr, &bounce, sizeof(bounce));
     }
 }
 
