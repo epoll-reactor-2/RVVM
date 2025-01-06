a11e6f036241f63d50ce304917b92403ffebe8cd
LekKit <50500857+LekKit@users.noreply.github.com>
2025-01-06 20:55:15 +0200

    rvvm_isolation: Fix build on arm32, fix SIGSYS crash with tcc
    - ARM32 doesn't define __NR_mmap, only __NR_mmap2 is used
    - Cover every other syscall in ifdefs just to be totally sure
    - Allow clone3() and a few other new useful syscalls
    - Don't use seccomp isolation with non-GNU compilers to prevent crashes on TCC and the likes

--- src/rvvm_isolation.c
+++ src/rvvm_isolation.c
@@ -36,7 +36,8 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #define ISOLATION_PRCTL_IMPL
 #endif
 
-#if defined(__linux__) && CHECK_INCLUDE(linux/seccomp.h, 0) && CHECK_INCLUDE(sys/prctl.h, 0)
+#if defined(GNU_EXTS) && !defined(SANITIZERS_PRESENT) && defined(__linux__) \
+ && CHECK_INCLUDE(linux/seccomp.h, 0) && CHECK_INCLUDE(sys/prctl.h, 0)
 #include <sys/mman.h>
 #include <sys/prctl.h>
 #include <linux/bpf.h>
@@ -110,6 +111,12 @@ static void drop_root_user(void)
 
 #if defined(ISOLATION_SECCOMP_IMPL) && !defined(SANITIZERS_PRESENT)
 
+#if defined(__riscv) && !defined(__NR_riscv_flush_icache)
+// Allow icache flush if no syscall definition is provided
+#define __NR_riscv_flush_icache 259
+#endif
+
+
 #define BPF_SECCOMP_ALLOW_SYSCALL(syscall) \
         BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, syscall, 0, 1), \
         BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW), \
@@ -141,14 +148,10 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
     struct sock_filter filter[] = {
         BPF_STMT(BPF_LD + BPF_W + BPF_ABS, offsetof(struct seccomp_data, nr)),
 
-#ifdef __NR_mmap2
-        BPF_SECCOMP_BLOCK_RWX_MMAN(__NR_mmap2) // i386-specific
-#endif
-        BPF_SECCOMP_BLOCK_RWX_MMAN(__NR_mmap)
-        BPF_SECCOMP_BLOCK_RWX_MMAN(__NR_mprotect)
-
         // Fast path exit for frequent syscalls
+#ifdef __NR_sched_yield
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sched_yield)
+#endif
 #ifdef __NR_futex
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_futex)
 #endif
@@ -168,10 +171,18 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_futex_waitv)
 #endif
 
+#ifdef __NR_pread64
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_pread64)
+#endif
+#ifdef __NR_pwrite64
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_pwrite64)
+#endif
+#ifdef __NR_sendto
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sendto)
+#endif
+#ifdef __NR_recvfrom
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_recvfrom)
+#endif
 #ifdef __NR_epoll_wait
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_epoll_wait)
 #endif
@@ -184,13 +195,23 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_epoll_ctl
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_epoll_ctl)
 #endif
+#ifdef __NR_clock_gettime
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_clock_gettime)
+#endif
+#ifdef __NR_nanosleep
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_nanosleep)
+#endif
+#ifdef __NR_read
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_read)
+#endif
+#ifdef __NR_write
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_write)
+#endif
 
         // Allow operations on already open fds
+#ifdef __NR_close
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_close)
+#endif
 #ifdef __NR_fstat
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_fstat)
 #endif
@@ -206,16 +227,43 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_lseek
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_lseek)
 #endif
+#ifdef __NR_munmap
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_munmap)
+#endif
+#ifdef __NR_brk
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_brk)
+#endif
+#ifdef __NR_rt_sigaction
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigaction)
+#endif
+#ifdef __NR_rt_sigprocmask
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigprocmask)
+#endif
+#ifdef __NR_rt_sigreturn
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigreturn)
+#endif
 
+        // Allow limited subset of mmap()/mprotect() (RWX is prohibited)
+#ifdef __NR_mmap2
+        BPF_SECCOMP_BLOCK_RWX_MMAN(__NR_mmap2) // Used by some 32-bit arches (i386, arm)
+#endif
+#ifdef __NR_mmap
+        BPF_SECCOMP_BLOCK_RWX_MMAN(__NR_mmap) // Missing on some 32-bit arches (riscv32, arm)
+#endif
+#ifdef __NR_mprotect
+        BPF_SECCOMP_BLOCK_RWX_MMAN(__NR_mprotect)
+#endif
+
+#ifdef __NR_ioctl
         // TODO: Research what malicious stuff can ioctl() do
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_ioctl)
+#endif
+#ifdef __NR_readv
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_readv)
+#endif
+#ifdef __NR_writev
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_writev)
+#endif
 #ifdef __NR_pipe
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_pipe)
 #endif
@@ -225,10 +273,18 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR__newselect
         BPF_SECCOMP_ALLOW_SYSCALL(__NR__newselect)
 #endif
+#ifdef __NR_mremap
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_mremap)
+#endif
+#ifdef __NR_msync
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_msync)
+#endif
+#ifdef __NR_mincore
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_mincore)
+#endif
+#ifdef __NR_madvise
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_madvise)
+#endif
 #ifdef __NR_dup
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_dup)
 #endif
@@ -247,7 +303,9 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_setitimer
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_setitimer)
 #endif
+#ifdef __NR_getpid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getpid)
+#endif
 #ifdef __NR_sendfile
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sendfile)
 #endif
@@ -256,33 +314,62 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #endif
 
         // Allow networking
+#ifdef __NR_socket
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_socket)
+#endif
+#ifdef __NR_connect
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_connect)
+#endif
 #ifdef __NR_accept
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_accept)
 #endif
 #ifdef __NR_socketcall
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_socketcall)
 #endif
+#ifdef __NR_sendmsg
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sendmsg)
+#endif
+#ifdef __NR_recvmsg
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_recvmsg)
+#endif
+#ifdef __NR_shutdown
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_shutdown)
+#endif
+#ifdef __NR_bind
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_bind)
+#endif
+#ifdef __NR_listen
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_listen)
+#endif
+#ifdef __NR_getsockname
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getsockname)
+#endif
+#ifdef __NR_getpeername
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getpeername)
+#endif
+#ifdef __NR_socketpair
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_socketpair)
+#endif
+#ifdef __NR_setsockopt
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_setsockopt)
+#endif
+#ifdef __NR_getsockopt
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getsockopt)
+#endif
 
-        // sys_clone() may be used for fork(), but all our isolation
-        // is inherited anyways
+        // sys_clone() may be used for fork(), but all our isolation is inherited anyways
+#ifdef __NR_clone
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_clone)
+#endif
+#ifdef __NR_exit
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_exit)
+#endif
 #ifdef __NR_wait4
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_wait4)
 #endif
+#ifdef __NR_uname
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_uname)
+#endif
 #ifdef __NR_shmctl
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_shmctl)
 #endif
@@ -295,15 +382,21 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_fcntl64
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_fcntl64)
 #endif
+#ifdef __NR_fsync
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_fsync)
+#endif
+#ifdef __NR_fdatasync
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_fdatasync)
+#endif
 #ifdef __NR_ftruncate
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_ftruncate)
 #endif
 #ifdef __NR_ftruncate64
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_ftruncate64)
 #endif
+#ifdef __NR_gettimeofday
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_gettimeofday)
+#endif
 #ifdef __NR_getrlimit
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getrlimit)
 #endif
@@ -313,11 +406,21 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_ugetrlimit
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_ugetrlimit)
 #endif
+#ifdef __NR_getrusage
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getrusage)
+#endif
+#ifdef __NR_sysinfo
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sysinfo)
+#endif
+#ifdef __NR_times
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_times)
+#endif
+#ifdef __NR_getuid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getuid)
+#endif
+#ifdef __NR_getgid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getgid)
+#endif
 #ifdef __NR_geteuid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_geteuid)
 #endif
@@ -342,14 +445,30 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_getsid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getsid)
 #endif
+#ifdef __NR_rt_sigpending
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigpending)
+#endif
+#ifdef __NR_rt_sigtimedwait
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigtimedwait)
+#endif
+#ifdef __NR_rt_sigqueueinfo
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigqueueinfo)
+#endif
+#ifdef __NR_rt_sigsuspend
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_sigsuspend)
+#endif
+#ifdef __NR_sigaltstack
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sigaltstack)
+#endif
+#ifdef __NR_sync
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sync)
+#endif
+#ifdef __NR_gettid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_gettid)
+#endif
+#ifdef __NR_readahead
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_readahead)
+#endif
 #ifdef __NR_time
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_time)
 #endif
@@ -383,15 +502,33 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_fadvise64
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_fadvise64)
 #endif
+#ifdef __NR_timer_create
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timer_create)
+#endif
+#ifdef __NR_timer_settime
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timer_settime)
+#endif
+#ifdef __NR_timer_gettime
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timer_gettime)
+#endif
+#ifdef __NR_timer_getoverrun
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timer_getoverrun)
+#endif
+#ifdef __NR_timer_delete
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timer_delete)
+#endif
+#ifdef __NR_clock_getres
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_clock_getres)
+#endif
+#ifdef __NR_clock_nanosleep
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_clock_nanosleep)
+#endif
+#ifdef __NR_exit_group
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_exit_group)
+#endif
+#ifdef __NR_tgkill
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_tgkill)
+#endif
 #ifdef __NR_waitid
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_waitid)
 #endif
@@ -413,23 +550,33 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_get_robust_list
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_get_robust_list)
 #endif
+#ifdef __NR_splice
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_splice)
+#endif
+#ifdef __NR_tee
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_tee)
+#endif
 #ifdef __NR_sync_file_range
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sync_file_range)
 #endif
+#ifdef __NR_vmsplice
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_vmsplice)
+#endif
 #ifdef __NR_move_pages
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_move_pages)
 #endif
 #ifdef __NR_signalfd
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_signalfd)
 #endif
+#ifdef __NR_timerfd_create
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timerfd_create)
+#endif
 #ifdef __NR_eventfd
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_eventfd)
 #endif
+#ifdef __NR_fallocate
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_fallocate)
+#endif
 #ifdef __NR_timerfd_settime
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_timerfd_settime)
 #endif
@@ -454,19 +601,27 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_pipe2
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_pipe2)
 #endif
+#ifdef __NR_preadv
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_preadv)
+#endif
+#ifdef __NR_pwritev
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_pwritev)
+#endif
 #ifdef __NR_rt_tgsigqueueinfo
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_rt_tgsigqueueinfo)
 #endif
+#ifdef __NR_recvmmsg
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_recvmmsg)
+#endif
 #ifdef __NR_prlimit64
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_prlimit64)
 #endif
 #ifdef __NR_syncfs
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_syncfs)
 #endif
+#ifdef __NR_sendmmsg
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_sendmmsg)
+#endif
 #ifdef __NR_getcpu
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_getcpu)
 #endif
@@ -500,16 +655,26 @@ static void seccomp_setup_syscall_filter(bool all_threads) {
 #ifdef __NR_io_uring_register
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_io_uring_register)
 #endif
+#ifdef __NR_clone3
+        // TODO: Research what malicious stuff can clone3() do
+        BPF_SECCOMP_ALLOW_SYSCALL(__NR_clone3)
+#endif
+#ifdef __NR_close_range
+        BPF_SECCOMP_ALLOW_SYSCALL(__NR_close_range)
+#endif
+#ifdef __NR_memfd_secret
+        BPF_SECCOMP_ALLOW_SYSCALL(__NR_memfd_secret)
+#endif
 #ifdef __NR_cachestat
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_cachestat)
+#endif
+#ifdef __NR_map_shadow_stack
+        BPF_SECCOMP_ALLOW_SYSCALL(__NR_map_shadow_stack)
 #endif
 
         // Arch-specific syscalls
 #ifdef __NR_riscv_flush_icache
         BPF_SECCOMP_ALLOW_SYSCALL(__NR_riscv_flush_icache)
-#elif defined(__riscv)
-        // Allow icache flush if no definition is provided
-        BPF_SECCOMP_ALLOW_SYSCALL(259)
 #endif
 
         // Return ENOSYS for everything not allowed here
