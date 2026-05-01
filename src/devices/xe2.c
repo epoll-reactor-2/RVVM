31bdeffb759306374235ec01d062de71eaced640
epoll-reactor-2 <163426089+epoll-reactor-2@users.noreply.github.com>
2026-05-01 21:44:32 +0200

    xe2: Assign display ID

--- src/devices/xe2.c
+++ src/devices/xe2.c
@@ -12,28 +12,39 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #include "devices/pci-bus.h"
 #include "mem_ops.h"
 #include "utils.h"
+#include <stdint.h>
+
+/* What is MTL?
+ * What is MCR? */
 
 #define xe2_reg_genmask(h, l)           (((~0u) << (l)) & (~0u >> (31 - (h))))
 #define xe2_reg_field_get(mask, val)    (((val) & (mask)) >> __builtin_ctz(mask))
 #define xe2_reg_field_prep(mask, val)   (((val) << __builtin_ctz(mask)) & (mask))
 
-#define XE2_VENDOR_ID_INTEL             0x8086
-#define XE2_DEVICE_ID_LUNAR_LAKE_IGPU   0x6420
-#define XE2_CLASS_CODE                  0x0300
+#define XE2_VENDOR_ID_INTEL                     0x8086
+#define XE2_DEVICE_ID_LUNAR_LAKE_IGPU           0x6420
+#define XE2_CLASS_CODE                          0x0300
 
-// 31        21              5    0
-// |   arch  |    release    | rev|
-// 00000000000000000000000000000000
-#define XE2_REG_GT_GMD_ID                   0xD8C
-#define XE2_REG_GT_GMD_ID_ARCH_MASK         xe2_reg_genmask(31, 22)
-#define XE2_REG_GT_GMD_ID_RELEASE_MASK      xe2_reg_genmask(21, 14)
-#define XE2_REG_GT_GMD_ID_REVID_MASK        xe2_reg_genmask(5, 0)
+#define XE2_REG_GT_GMD_ID                       0xD8C
+#define XE2_REG_GT_GMD_ID_ARCH_MASK             xe2_reg_genmask(31, 22)
+#define XE2_REG_GT_GMD_ID_RELEASE_MASK          xe2_reg_genmask(21, 14)
+#define XE2_REG_GT_GMD_ID_REVID_MASK            xe2_reg_genmask(5, 0)
 
-#define XE2_REG_GT_FORCEWAKE_ACK_GSC        0xDF8
-#define XE2_REG_GT_FORCEWAKE_ACK_GT_MTL     0xDFC
+#define XE2_REG_GT_GMD_ID_DISPLAY               0x510A0
+#define XE2_REG_GT_GMD_ID_DISPLAY_ARCH_MASK     xe2_reg_genmask(31, 22)
+#define XE2_REG_GT_GMD_ID_DISPLAY_RELEASE_MASK  xe2_reg_genmask(21, 14)
+#define XE2_REG_GT_GMD_ID_DISPLAY_REVID_MASK    xe2_reg_genmask(5, 0)
+
+#define XE2_REG_GT_FORCEWAKE_GSC                0xA618
+#define XE2_REG_GT_FORCEWAKE_ACK_GSC            0xDF8
+#define XE2_REG_GT_FORCEWAKE_GT                 0xA188
+#define XE2_REG_GT_FORCEWAKE_ACK_GT             0x130044 // How these forcewakes are related?
+#define XE2_REG_GT_FORCEWAKE_ACK_GT_MTL         0xDFC
 
 typedef struct {
     pci_func_t *pci_func;
+    uint32_t    forcewake_gsc;
+    uint32_t    forcewake_gt_mtl;
 } xe2_dev_t;
 
 static void xe2_remove(rvvm_mmio_dev_t* dev)
@@ -46,42 +57,80 @@ static rvvm_mmio_type_t xe2_type = {
     .remove = xe2_remove,
 };
 
-static bool xe2_mmio_read(rvvm_mmio_dev_t* dev, void* data, size_t offset, uint8_t size)
+static bool xe2_mmio_read(rvvm_mmio_dev_t *dev, void *data, size_t offset, uint8_t size)
 {
+    UNUSED(size);
     rvvm_info("PCI read: offset=%lx, data=%x", offset, read_uint32_le(data));
 
+    xe2_dev_t *xe2 = dev->data;
+
     switch (offset) {
-    case XE2_REG_GT_GMD_ID: {
-        uint32_t cmd = xe2_reg_field_prep(XE2_REG_GT_GMD_ID_ARCH_MASK, 20)
-                     | xe2_reg_field_prep(XE2_REG_GT_GMD_ID_RELEASE_MASK, 1)
-                     | xe2_reg_field_prep(XE2_REG_GT_GMD_ID_REVID_MASK, 1);
-        write_uint32_le(data, cmd);
-        break;
-    }
-    default:
-        break;
+        case XE2_REG_GT_GMD_ID: {
+            uint32_t cmd = xe2_reg_field_prep(XE2_REG_GT_GMD_ID_ARCH_MASK, 20)
+                         | xe2_reg_field_prep(XE2_REG_GT_GMD_ID_RELEASE_MASK, 1)
+                         | xe2_reg_field_prep(XE2_REG_GT_GMD_ID_REVID_MASK, 1);
+            write_uint32_le(data, cmd);
+            break;
+        }
+        case XE2_REG_GT_GMD_ID_DISPLAY: {
+            // i915/display/intel_display_device.c:
+            // static const struct {
+            //      u16 ver;
+            //      u16 rel;
+            //      const struct intel_display_device_info *display;
+            //  gmdid_display_map[] = {
+            //      { 14,  0, &xe_lpdp_display },
+            //      { 14,  1, &xe2_hpd_display },
+            //      { 20,  0, &xe2_lpd_display },
+            //      { 30,  0, &xe2_lpd_display },
+            //      { 30,  2, &xe2_lpd_display },
+            // };
+            uint32_t cmd = xe2_reg_field_prep(XE2_REG_GT_GMD_ID_DISPLAY_ARCH_MASK, 14)
+                         | xe2_reg_field_prep(XE2_REG_GT_GMD_ID_DISPLAY_RELEASE_MASK, 1)
+                         | xe2_reg_field_prep(XE2_REG_GT_GMD_ID_DISPLAY_REVID_MASK, 0);
+            write_uint32_le(data, cmd);
+            break;
+        }
+        case XE2_REG_GT_FORCEWAKE_ACK_GSC:
+            write_uint32_le(data, xe2->forcewake_gsc);
+            break;
+        case XE2_REG_GT_FORCEWAKE_ACK_GT_MTL:
+        case XE2_REG_GT_FORCEWAKE_ACK_GT:
+            write_uint32_le(data, xe2->forcewake_gt_mtl);
+            break;
+        default:
+            break;
     }
 
-    UNUSED(dev);
-    UNUSED(data);
-    UNUSED(offset);
-    UNUSED(size);
     return true;
 }
 
-static bool xe2_mmio_write(rvvm_mmio_dev_t* dev, void* data, size_t offset, uint8_t size)
+static bool xe2_mmio_write(rvvm_mmio_dev_t *dev, void *data, size_t offset, uint8_t size)
 {
-    rvvm_info("PCI write: offset=%lx, data=%x", offset, read_uint32_le(data));
-    UNUSED(dev);
-    UNUSED(data);
-    UNUSED(offset);
     UNUSED(size);
+    rvvm_info("PCI write: offset=%lx, data=%x", offset, read_uint32_le(data));
+
+    xe2_dev_t *xe2 = dev->data;
+
+    switch (offset) {
+        case XE2_REG_GT_FORCEWAKE_GSC:
+            xe2->forcewake_gsc = read_uint32_le(data);
+            break;
+        case XE2_REG_GT_FORCEWAKE_GT:
+            xe2->forcewake_gt_mtl = read_uint32_le(data);
+            break;
+        default:
+            break;
+    }
+
     return true;
 }
 
 PUBLIC pci_dev_t *xe2_init(pci_bus_t *pci_bus)
 {
     xe2_dev_t *xe2 = safe_new_obj(xe2_dev_t);
+    xe2->forcewake_gsc = 0;
+    xe2->forcewake_gt_mtl = 0;
 
     pci_func_desc_t xe2_desc = {
         .vendor_id  = XE2_VENDOR_ID_INTEL,
