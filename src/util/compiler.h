c573aa238e65bdb5511761cf49f80dcfe149fad0
LekKit <50500857+LekKit@users.noreply.github.com>
2026-07-18 18:14:51 +0300

    compiler: Include <stdbool.h>, <stddef.h>

--- src/util/compiler.h
+++ src/util/compiler.h
@@ -7,9 +7,11 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-#ifndef LEKKIT_COMPILER_TRICKS_H
-#define LEKKIT_COMPILER_TRICKS_H
+#ifndef RVVM_UTIL_COMPILER_TRICKS_H
+#define RVVM_UTIL_COMPILER_TRICKS_H
 
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 /*
